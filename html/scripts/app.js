99da7e1b1be61a5b0ef06fd12edebac269174d2c
Alexander <maslov-composer@yandex.ru>
2018-11-02 19:55:28 +0300

    progress

--- html/scripts/app.js
+++ html/scripts/app.js
@@ -1,3 +1,8 @@
 import MobileMenu from './MobileMenu';
+import StickyHeader from './StickyHeader';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
+
+console.log(stickyHeader.siteHeader);
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,11 @@ module.exports = {
 		rules: [
 			{ 
 				test: /\.js$/, 
+				exclude: /(node_modules|bower_components)/,
 				use: [
 					{
 						loader: "babel-loader",
-						options: { "presets": ["@babel/preset-env"] } 
+						options: { "presets": ["@babel/preset-env"] }
 					}
 				]
 			}
