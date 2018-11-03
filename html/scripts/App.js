a372d5d5c71899dc8a4ac335258d94f94e6c93c5
Alexander <maslov-composer@yandex.ru>
2018-11-03 13:26:34 +0300

    add App.js

---
+++ html/scripts/App.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import MobileMenu from './MobileMenu';
+import StickyHeader from './StickyHeader';
+import Modal from './Modal';
+
+var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
