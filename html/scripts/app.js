ae8e7c5d0276c268803bb2bccb72954077c687e6
Alexander <maslov-composer@yandex.ru>
2018-11-03 13:25:25 +0300

    delete app

--- html/scripts/app.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import MobileMenu from './MobileMenu';
-import StickyHeader from './StickyHeader';
-import Modal from './Modal';
-
-var mobileMenu = new MobileMenu();
-var stickyHeader = new StickyHeader();
-var modal = new Modal();
